8822bda0f3812c8c949d81168d6c96c7a79efcb7
frandell11 <delloliofranco2@gmail.com>
2024-06-30 22:19:22 -0300

    Add files via upload

---
+++ funciones.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include "funciones.h"
+
+using namespace std;
+
+
+
+    int tirada(){
+        return rand ()% 6+1;
+    }
+
+    void hacerTirada(int dado[6]){
+        int y;
+    for(y=0; y<6; y++){
+        dado[y] = tirada();
+      }
+    }
+
+    void mostrarTirada(int dado[6]){
+    int y;
+
+    for (y=0; y<6; y++){
+    cout << dado[y] << endl;
+    }
+    }
+
+    int sexteto(int dado[6]){
+    int y;
+    int rep = 0;
+    int numRep;
+
+    for(int y=0; y<6; y++){
+        dado[y] = tirada();
+
+        if(y == dado[y]){
+            rep++;
+        }
+        else if(rep == 6){
+            numRep = y;
+        }
+      }
+    }
+
+    int puntajeMaxRonda(int tiradaA, int tiradaB, int tiradaC){
+    int PuntajeRonda;
+
+    if(tiradaA > tiradaB && tiradaA > tiradaC){
+        PuntajeRonda = tiradaA;
+    }
+    else if(tiradaB > tiradaA && tiradaB > tiradaC){
+        PuntajeRonda = tiradaB;
+    }
+    else{
+        PuntajeRonda = tiradaC;
+    }
+    return PuntajeRonda;
+    cout << "el puntaje total de la ronda es: " << PuntajeRonda << endl;
+    cout << endl;
+    system("pause");
+    system("cls");
+    }
+
+
+
+
---
+++ juego.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "funciones.h"
+#include "juego.h"
+
+using namespace std;
+
+void juego(){
+
+
+int dado[6];
+int PuntajeJuegoTotal = 0;
+
+while(PuntajeJuegoTotal < 100){
+
+for(int x=0; x<3; x++){
+
+
+//generar tirada//
+
+hacerTirada(dado);
+
+//mostrar tirada//
+
+mostrarTirada(dado);
+}
+}
+}
+
+
